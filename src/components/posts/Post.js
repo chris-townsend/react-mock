3070016e6a99c213b98571d984063f5c2089c367
chris-townsend <chris-townsend@live.co.uk>
2023-02-03 15:24:35 +0000

    Style Post.js

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,4 +1,5 @@
 import "./Post.css"
+import { MoreVertOutlined } from "@mui/icons-material"
 
 export default function Post() {
   return (
@@ -11,9 +12,23 @@ export default function Post() {
                     <span className="postDate">5 mins ago</span>
                 </div>
                 <div className="postTopRight"></div>
+                <MoreVertOutlined />
             </div>
-            <div className="postCenter"></div>
-            <div className="postBottom"></div>
+            <div className="postCenter">
+              <span className="postText">Hey, this is my first post!</span>
+              <img className="postImg" src="/assets/images/posts/post-1.jpg" alt="" />
+            </div>
+            <div className="postBottom">
+              <div className="postBottomLeft">
+                <img className="likeIcon" src="/assets/images/posts/like-button.png" alt="" />
+                <img className="likeIcon" src="/assets/images/posts/heart-button.png" alt="" />
+                <span className="postLikeCounter">42 people liked this</span>
+              </div>
+              <div className="postBottomRight">
+                <span className="postCommentText">9 comments</span>
+
+              </div>
+            </div>
         </div>
         
     </div>
